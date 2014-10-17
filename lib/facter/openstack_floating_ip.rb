61cddd34894b213baaa8573221d37845d5856a68
Stefan Andres <s.andres@syseleven.de>
2014-10-17 14:55:05 +0200

    fail silently when /config is not there

--- lib/facter/openstack_floating_ip.rb
+++ lib/facter/openstack_floating_ip.rb
@@ -2,9 +2,9 @@ Facter.add('openstack_floating_ip') do
   setcode do
     require 'json'
 
-    data = JSON.parse(IO.read('/config/openstack/latest/meta_data.json'))
     begin
-      data['meta']['floating_ip']
+    data = JSON.parse(IO.read('/config/openstack/latest/meta_data.json'))
+    data['meta']['floating_ip']
     rescue
       'false'
     end
