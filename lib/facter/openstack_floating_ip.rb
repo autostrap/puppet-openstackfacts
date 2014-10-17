13944c7e0e4236b367df454792ea92e507798b07
Stefan Andres <s.andres@syseleven.de>
2014-10-17 15:10:43 +0200

    return empty string on error

--- lib/facter/openstack_floating_ip.rb
+++ lib/facter/openstack_floating_ip.rb
@@ -6,7 +6,7 @@ Facter.add('openstack_floating_ip') do
     data = JSON.parse(IO.read('/config/openstack/latest/meta_data.json'))
     data['meta']['floating_ip']
     rescue
-      'false'
+      ''
     end
   end
 end
