f30b0157c4decb036b9419b2d053174f512e0648
Stefan Andres <s.andres@syseleven.de>
2015-03-05 10:22:36 +0100

    os-423 add ec2_public_ipv4 fact as fallback for openstack_floating_ip

--- lib/facter/openstack_floating_ip.rb
+++ lib/facter/openstack_floating_ip.rb
@@ -2,11 +2,17 @@ Facter.add('openstack_floating_ip') do
   setcode do
     require 'json'
 
+    floatingip = nil
+
     begin
-    data = JSON.parse(IO.read('/config/openstack/latest/meta_data.json'))
-    data['meta']['floating_ip']
+      data = JSON.parse(IO.read('/config/openstack/latest/meta_data.json'))
+      floatingip = data['meta']['floating_ip']
     rescue
-      ''
     end
+
+    if ! floatingip
+      floatingip = Facter.value(:ec2_public_ipv4)
+    end
+    floatingip
   end
 end
