cd22242b62dba9c92627f3989c7682daaaabd123
Johannes Grassler <j.grassler@syseleven.de>
2015-03-17 15:37:39 +0100

    os-464 Fixed indentation.

--- lib/facter/nodetype.rb
+++ lib/facter/nodetype.rb
@@ -3,8 +3,8 @@ Facter.add('nodetype') do
     require 'json'
 
     begin
-    data = JSON.parse(IO.read('/config/openstack/latest/meta_data.json'))
-    data['meta']['nodetype']
+      data = JSON.parse(IO.read('/config/openstack/latest/meta_data.json'))
+      data['meta']['nodetype']
     rescue
       ''
     end
