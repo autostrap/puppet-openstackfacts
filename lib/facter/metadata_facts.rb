e3321b67ff7b08ef93fff31671d2b577f87e8e78
Johannes Grassler <j.grassler@syseleven.de>
2015-06-19 16:47:48 +0200

    os-599 Added metadata_facts.rb for facts that can be taken from metadata verbatim and created sshkeys fact.

---
+++ lib/facter/metadata_facts.rb
@@ -0,0 +1,18 @@
+# This file contains all facts that can be retrieved as-is from cloud-init
+# metadata. Just add the metadata keys to the array at the top.
+['nodetype',
+ 'stack_name',
+ 'sshkeys'].each { |factname|
+    Facter.add(factname) do
+      setcode do
+        require 'json'
+
+        begin
+          data = JSON.parse(IO.read('/config/openstack/latest/meta_data.json'))
+          data['meta'][factname]
+        rescue
+          ''
+        end
+      end
+    end
+ }
