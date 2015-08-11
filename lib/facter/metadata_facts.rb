7fd57e36a73913acd8210c4002d95227a7209440
Stefan Andres <s.andres@syseleven.de>
2015-08-11 15:24:10 +0200

    os-821, add cloudname fact

--- lib/facter/metadata_facts.rb
+++ lib/facter/metadata_facts.rb
@@ -1,6 +1,7 @@
 # This file contains all facts that can be retrieved as-is from cloud-init
 # metadata. Just add the metadata keys to the array at the top.
 ['nodetype',
+ 'cloudname'
  'stack_name',
  'sshkeys'].each { |factname|
     Facter.add(factname) do
